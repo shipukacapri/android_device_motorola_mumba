4b6093aa33a6e7665999cb97f6eb294d30a1cf22
kleidione Freitas <kleidione@gmail.com>
2026-07-24 08:47:52 -0300

    mumba: sepolicy: Silence hal_audio_default default_prop denial
    - avc:  denied  { read } for  comm="writer" name="u:object_r:default_prop:s0" dev="tmpfs" ino=181 scontext=u:r:hal_audio_default:s0 tcontext=u:object_r:default_prop:s0 tclass=file permissive=0
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
